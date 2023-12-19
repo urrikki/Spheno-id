cb81629c84db6a1f1de7283ffd047e59b9eabca9
Tristan BODA <115488503+Kron0l@users.noreply.github.com>
2023-12-19 18:07:24 +0100

    indentation

--- script.js
+++ script.js
@@ -9,220 +9,219 @@ var timer;
 var interface = document.getElementById("controls-zone");
 const loader = document.querySelector('.loader');
 
-    async function init() {
-      await initSession(); 
-      await initEntities(); 
+async function init() {
+  await initSession(); 
+  await initEntities(); 
+}
+
+async function onFirstFrame() {
+  loader.classList.add('fondu-out');
+  SDK3DVerse.notifier.off('onFramePreRender', onFirstFrame);
+}
+
+async function initSession() {
+
+  await SDK3DVerse.joinOrStartSession({
+    userToken: "public_VU0u4tvRqwxLIiZw",
+    sceneUUID: "e6d5bd66-be7f-47eb-8569-5cf441abfd7c",
+    canvas: document.getElementById("display-canvas"),
+    viewportProperties: {
+      defaultControllerType: SDK3DVerse.controller_type.orbit,
+    },
+  });
+  SDK3DVerse.notifier.on('onFramePreRender', onFirstFrame);
+}
+
+async function initEntities() {
+  const entities = await SDK3DVerse.engineAPI.findEntitiesByEUID("8dda24fe-0d46-427a-ab54-a8771eca876f");
+  linker = entities[0];
+  linker.setVisibility(false);
+  
+  const hEntities = await SDK3DVerse.engineAPI.findEntitiesByEUID("fa9f5c6e-f595-47a8-8d96-13e57d5db262");
+  hlinker = hEntities[0];
+
+  lteeth = await SDK3DVerse.engineAPI.findEntitiesByNames('lteeth');
+  lgum = await SDK3DVerse.engineAPI.findEntitiesByNames('lgum');
+  lmuscle = await SDK3DVerse.engineAPI.findEntitiesByNames('lmuscle');
+  hteeth = await SDK3DVerse.engineAPI.findEntitiesByNames('hteeth');
+  hgum = await SDK3DVerse.engineAPI.findEntitiesByNames('hgum');
+  hmuscle = await SDK3DVerse.engineAPI.findEntitiesByNames('hmuscle');
+
+  lElements = [lteeth, lgum, lmuscle];
+  hElements = [hteeth, hgum, hmuscle];
+
+}
+
+function opacity(Elements, theOneId , opacityValue , reset) {
+  if (reset == true)
+  {
+    opacity(Elements , 'ok' , 1 , false);
+  }
+  Elements.forEach(element => {
+    const entity = element[0];
+    if (entity?.getID() === theOneId) {
+      return;
     }
+    let materialDataJSON = entity.getComponent('material').dataJSON;
+    materialDataJSON = SDK3DVerse.utils.clone(materialDataJSON);
+    materialDataJSON.opacity = opacityValue;
+    entity.setComponent('material', { dataJSON : materialDataJSON });
+  })
+}
 
-    async function onFirstFrame() {
-      loader.classList.add('fondu-out');
-      SDK3DVerse.notifier.off('onFramePreRender', onFirstFrame);
-    }
+function comeBack()
+{
+  if (linker.isVisible())
+  {
+    opacity(lElements , 'ok' , 1 , false)
+  }
+  else
+  {
+    opacity(hElements , 'ok' , 1 , false)
+  }
+}
 
-    async function initSession() {
+function togglePlayPause() {
+  var startPauseButton = document.getElementById("startPauseButton");
 
-      await SDK3DVerse.joinOrStartSession({
-        userToken: "public_VU0u4tvRqwxLIiZw",
-        sceneUUID: "e6d5bd66-be7f-47eb-8569-5cf441abfd7c",
-        canvas: document.getElementById("display-canvas"),
-        viewportProperties: {
-          defaultControllerType: SDK3DVerse.controller_type.orbit,
-        },
-      });
-      SDK3DVerse.notifier.on('onFramePreRender', onFirstFrame);
-    }
-
-    async function initEntities() {
-      const entities = await SDK3DVerse.engineAPI.findEntitiesByEUID("8dda24fe-0d46-427a-ab54-a8771eca876f");
-      linker = entities[0];
-      linker.setVisibility(false);
-      
-      const hEntities = await SDK3DVerse.engineAPI.findEntitiesByEUID("fa9f5c6e-f595-47a8-8d96-13e57d5db262");
-      hlinker = hEntities[0];
-
-      lteeth = await SDK3DVerse.engineAPI.findEntitiesByNames('lteeth');
-      lgum = await SDK3DVerse.engineAPI.findEntitiesByNames('lgum');
-      lmuscle = await SDK3DVerse.engineAPI.findEntitiesByNames('lmuscle');
-      hteeth = await SDK3DVerse.engineAPI.findEntitiesByNames('hteeth');
-      hgum = await SDK3DVerse.engineAPI.findEntitiesByNames('hgum');
-      hmuscle = await SDK3DVerse.engineAPI.findEntitiesByNames('hmuscle');
-
-      lElements = [lteeth, lgum, lmuscle];
-      hElements = [hteeth, hgum, hmuscle];
-
-    }
-
-    function opacity(Elements, theOneId , opacityValue , reset) {
-      if (reset == true)
-      {
-        opacity(Elements , 'ok' , 1 , false);
-      }
-      Elements.forEach(element => {
-          const entity = element[0];
-          if (entity?.getID() === theOneId) {
-              return;
-          }
-          let materialDataJSON = entity.getComponent('material').dataJSON;
-          materialDataJSON = SDK3DVerse.utils.clone(materialDataJSON);
-          materialDataJSON.opacity = opacityValue;
-          entity.setComponent('material', { dataJSON : materialDataJSON });
-      })
+  if (isPlaying) {
+    SDK3DVerse.engineAPI.pauseAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" , hlinker);
+    SDK3DVerse.engineAPI.pauseAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0" , linker );
+    startPauseButton.innerHTML = '<i class="far fa-circle-play"></i>';
+  } else {
+    // Animation
+    SDK3DVerse.engineAPI.playAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" , {playbackSpeed : 1} , hlinker);
+    SDK3DVerse.engineAPI.playAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0", {playbackSpeed : 1} , linker);
+    startPauseButton.innerHTML = '<i class="far fa-circle-pause"></i>';
   }
 
-    function comeBack()
-    {
-      if (linker.isVisible())
-      {
-        opacity(lElements , 'ok' , 1 , false)
-      }
-      else
-      {
-        opacity(hElements , 'ok' , 1 , false)
-      }
-    }
+  isPlaying = !isPlaying;
+}
 
-    function togglePlayPause() {
-      var startPauseButton = document.getElementById("startPauseButton");
+function reverse() {
+  SDK3DVerse.engineAPI.playAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" ,{ playbackSpeed : -1 } , hlinker);
+  SDK3DVerse.engineAPI.playAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0" , { playbackSpeed : -1 } , linker);
+  if (isPlaying == false)
+  {
+    startPauseButton.innerHTML = '<i class="far fa-circle-pause"></i>';
+    isPlaying = true;
+  }
+}
+
+function forward() {
+  SDK3DVerse.engineAPI.playAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" ,{ playbackSpeed : 2 } , hlinker);
+  SDK3DVerse.engineAPI.playAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0" , { playbackSpeed : 2 }, linker);
+  if (isPlaying == false)
+  {
+    startPauseButton.innerHTML = '<i class="far fa-circle-pause"></i>';
+    isPlaying = true;
+  }
+}
+
+    
+function openOptions(){
+  if (linker.isVisible())
+  {
+    linker.setVisibility(false);
+    hlinker.setVisibility(true);
+  }
+  else
+  {
+    linker.setVisibility(true);
+    hlinker.setVisibility(false);
+  }
+}
+
+function handleMouseMove() {
+  interface.style.visibility = "visible";
+  resetTimeout();
+}
+    
+function resetTimeout() {
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    interface.style.visibility = "hidden";
+  }, 5000); 
+}
+
+async function onClick(event) {
+  const target = await SDK3DVerse.engineAPI.castScreenSpaceRay(
+    event.clientX,
+    event.clientY 
+  );
   
-      if (isPlaying) {
-        SDK3DVerse.engineAPI.pauseAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" , hlinker);
-        SDK3DVerse.engineAPI.pauseAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0" , linker );
-        startPauseButton.innerHTML = '<i class="far fa-circle-play"></i>';
-      } else {
-        // Animation
-        SDK3DVerse.engineAPI.playAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" , {playbackSpeed : 1} , hlinker);
-        SDK3DVerse.engineAPI.playAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0", {playbackSpeed : 1} , linker);
-        startPauseButton.innerHTML = '<i class="far fa-circle-pause"></i>';
-      }
+  if (!target.pickedPosition) return;
+  const clickedEntity = target.entity;
 
-      isPlaying = !isPlaying;
+  if (linker.isVisible())
+  {
+    if (clickedEntity.getID() == lteeth[0]?.getID()) {
+      opacity(lElements , lteeth[0]?.getID() , 0.1 , true)
     }
-
-    function reverse() {
-      SDK3DVerse.engineAPI.playAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" ,{ playbackSpeed : -1 } , hlinker);
-      SDK3DVerse.engineAPI.playAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0" , { playbackSpeed : -1 } , linker);
-      if (isPlaying == false)
-      {
-        startPauseButton.innerHTML = '<i class="far fa-circle-pause"></i>';
-        isPlaying = true;
-      }
+    else if (clickedEntity.getID() == lgum[0]?.getID()){
+      opacity(lElements , lgum[0]?.getID(), 0.1, true)
     }
-
-    function forward() {
-      SDK3DVerse.engineAPI.playAnimationSequence("22a4a489-f0fc-449b-aa87-81d59cc31d6e" ,{ playbackSpeed : 2 } , hlinker);
-      SDK3DVerse.engineAPI.playAnimationSequence("6bbf5e8c-d3ea-4f44-8c04-c477955bc8b0" , { playbackSpeed : 2 }, linker);
-      if (isPlaying == false)
-      {
-        startPauseButton.innerHTML = '<i class="far fa-circle-pause"></i>';
-        isPlaying = true;
-      }
+    else if (clickedEntity.getID() == lmuscle[0]?.getID()){
+      opacity(lElements , lmuscle[0]?.getID(), 0.1, true)
     }
+  }
+  else if (hlinker.isVisible())
+  {
+    if (clickedEntity.getID() == hteeth[0]?.getID()) {
+      opacity(hElements , hteeth[0]?.getID(), 0.1, true)
+    }
+    else if (clickedEntity.getID() == hgum[0]?.getID()){
+      opacity(hElements , hgum[0]?.getID(), 0.1, true)
+    }
+    else if (clickedEntity.getID() == hmuscle[0]?.getID()){
+      opacity(hElements , hmuscle[0]?.getID(), 0.1, true)
+    }
+  }
+};
 
+const entityNames = {
+  'None': '',
+  0: 'Teeth',
+  1: 'Gum',
+  2: 'Muscle',
+};
     
-    function openOptions(){
-      if (linker.isVisible())
-      {
-        linker.setVisibility(false);
-        hlinker.setVisibility(true);
-      }
-      else
-      {
-        linker.setVisibility(true);
-        hlinker.setVisibility(false);
-      }
-    }
-
-    function handleMouseMove() {
-      interface.style.visibility = "visible";
-      resetTimeout();
-    }
+function toggleDropdown() {
+  const dropdownOptions = document.querySelector('.dropdown-options');
+  dropdownOptions.classList.toggle('hidden');
+}
     
-    function resetTimeout() {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        interface.style.visibility = "hidden";
-      }, 5000); 
-    }
+function handleOptionSelection(event) {
+  const selectedValue = event.target.getAttribute('data-value');
+  const selectedName = entityNames[selectedValue];
 
-    async function onClick(event) {
-      const target = await SDK3DVerse.engineAPI.castScreenSpaceRay(
-        event.clientX,
-        event.clientY 
-      );
-      
-      if (!target.pickedPosition) return;
-      const clickedEntity = target.entity;
-    
-      if (linker.isVisible())
-      {
-        if (clickedEntity.getID() == lteeth[0]?.getID()) {
-          opacity(lElements , lteeth[0]?.getID() , 0.1 , true)
-        }
-        else if (clickedEntity.getID() == lgum[0]?.getID()){
-          opacity(lElements , lgum[0]?.getID(), 0.1, true)
-        }
-        else if (clickedEntity.getID() == lmuscle[0]?.getID()){
-          opacity(lElements , lmuscle[0]?.getID(), 0.1, true)
-        }
-      }
-      else if (hlinker.isVisible())
-      {
-        if (clickedEntity.getID() == hteeth[0]?.getID()) {
-          opacity(hElements , hteeth[0]?.getID(), 0.1, true)
-        }
-        else if (clickedEntity.getID() == hgum[0]?.getID()){
-          opacity(hElements , hgum[0]?.getID(), 0.1, true)
-        }
-        else if (clickedEntity.getID() == hmuscle[0]?.getID()){
-          opacity(hElements , hmuscle[0]?.getID(), 0.1, true)
-        }
-      }
-    };
-
-    const entityNames = {
-      'None': '',
-      0: 'Teeth',
-      1: 'Gum',
-      2: 'Muscle',
-    };
-    
-    function toggleDropdown() {
-      const dropdownOptions = document.querySelector('.dropdown-options');
-      dropdownOptions.classList.toggle('hidden');
+  document.querySelector('.dropdown-header').innerHTML = `Models : ${selectedName} &#9660;`;
+  if (selectedValue == "None")
+  {
+    comeBack();
+  }
+  else
+  {
+    if (linker.isVisible())
+    {
+      opacity(lElements , getIDentity(lElements , selectedValue) , 0.3, true);
     }
-    
-    function handleOptionSelection(event) {
-      const selectedValue = event.target.getAttribute('data-value');
-      const selectedName = entityNames[selectedValue];
-
-      document.querySelector('.dropdown-header').innerHTML = `Models : ${selectedName} &#9660;`;
-      if (selectedValue == "None")
-      {
-        comeBack();
-      }
-      else
-      {
-        if (linker.isVisible())
-        {
-          opacity(lElements , getIDentity(lElements , selectedValue) , 0.3, true);
-        }
-        else
-        {
-          opacity(hElements , getIDentity(hElements , selectedValue) , 0.3, true);
-        }
-      }
+    else
+    {
+      opacity(hElements , getIDentity(hElements , selectedValue) , 0.3, true);
     }
+  }
+}
 
-    function getIDentity(Elements , Number) {
-      return Elements[Number][0]?.getID();
-    }
+function getIDentity(Elements , Number) {
+  return Elements[Number][0]?.getID();
+}
 
-    function toggleSidenav() {
-      var navbar = document.getElementById("sidenav");
-      if (navbar.style.left === "0px") {
-        navbar.style.left = "-250px";
-      } else {
-        navbar.style.left = "0px";
-      }
-    }
-    
+function toggleSidenav() {
+  var navbar = document.getElementById("sidenav");
+  if (navbar.style.left === "0px") {
+    navbar.style.left = "-250px";
+  } else {
+    navbar.style.left = "0px";
+  }
+}
